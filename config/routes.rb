01d56ae8038930505b456d4f504f1ff26233ef06
costelinha <andregomesngr@gmail.com>
2020-08-13 15:04:59 -0500

    set campaign and member routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  get 'members/create'
-  get 'members/destroy'
-  get 'members/update'
-  get 'campaigns/show'
-  get 'campaigns/index'
-  get 'campaigns/create'
-  get 'campaigns/update'
-  get 'campaigns/destroy'
-  get 'campaigns/raffle'
-  get 'pages/home'
   devise_for :users, :controllers => { registrations: 'registrations' }
   mount Sidekiq::Web => '/sidekiq'
+
+  resources :campaigns, except: [:new] do 
+    post 'raffle', on: :member 
+    #* other option to route
+    # post 'raffle', on: :colection
+  end
+
+  get 'members/:token/opened', to: 'member#opened'
+  resources :members, only: [:create, :destroy, :update]
+
+  root to: 'pages#home'
 end

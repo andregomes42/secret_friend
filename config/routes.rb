589de51f1c834806ee677872a3d6c5087836549f
costelinha <andregomesngr@gmail.com>
2020-08-25 16:40:32 -0500

    production environment config

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  mount Sidekiq::Web => '/sidekiq'
+  #mount Sidekiq::Web => '/sidekiq'
 
   resources :campaigns, except: [:new] do 
     post 'raffle', on: :member 
